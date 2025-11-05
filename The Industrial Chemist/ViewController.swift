167386d5162a487a9cbadec577ab9c8e6a311fe0
anshggss <ptmtripathi@gmail.com>
2025-11-05 08:05:03 +0530

    Initial Commit

---
+++ The Industrial Chemist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Industrial Chemist
+//
+//  Created by admin25 on 05/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
