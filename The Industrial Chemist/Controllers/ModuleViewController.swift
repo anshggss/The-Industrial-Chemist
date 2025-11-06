ac742991bf2562ecb53e8099439c8a24575a60a7
Anushka1804 <anushkaagarwal180405@gmail.com>
2025-11-06 18:59:26 +0530

    created modules view controller

---
+++ The Industrial Chemist/Controllers/ModuleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ModuleViewController.swift
+//  The Industrial Chemist
+//
+//  Created by user@7 on 06/11/25.
+//
+
+import UIKit
+
+class ModuleViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
