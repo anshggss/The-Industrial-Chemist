852698ba8b9ab11c125b009352a9cf434eda272c
anshggss <ptmtripathi@gmail.com>
2025-11-07 10:29:23 +0530

    Added View Controller for profile.xib

--- The Industrial Chemist/Controllers/ProfileViewController.swift
+++ The Industrial Chemist/Controllers/ProfileViewController.swift
@@ -2,18 +2,27 @@
 //  ProfileViewController.swift
 //  The Industrial Chemist
 //
-//  Created by user@7 on 06/11/25.
+//  Created by admin25 on 07/11/25.
 //
 
 import UIKit
 
-class ProfileViewController: UIView {
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
 
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
     */
 
