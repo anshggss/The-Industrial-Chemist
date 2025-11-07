9e74fe7f078537ee48ddf5661b67e0b44fd65d23
anshggss <ptmtripathi@gmail.com>
2025-11-07 09:42:03 +0530

    Resolved merge conflict in Xcode workspace state and updated storyboard

---
+++ The Industrial Chemist/Controllers/EditDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditDetailsViewController.swift
+//  The Industrial Chemist
+//
+//  Created by user@14 on 07/11/25.
+//
+
+import UIKit
+
+class EditDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
