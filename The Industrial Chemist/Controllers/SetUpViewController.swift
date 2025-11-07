75000f64ff9ca628aa149013a2245e9f129f8fc8
Anushka1804 <anushkaagarwal180405@gmail.com>
2025-11-07 22:08:10 +0530

    created and made the setup page

---
+++ The Industrial Chemist/Controllers/SetUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SetUpViewController.swift
+//  The Industrial Chemist
+//
+//  Created by user@7 on 07/11/25.
+//
+
+import UIKit
+
+class SetUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
