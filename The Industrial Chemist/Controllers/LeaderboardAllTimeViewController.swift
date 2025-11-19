88816e6345f981814d31aa0ac0b88f0d91daa92c
anshggss <ptmtripathi@gmail.com>
2025-11-19 12:55:29 +0530

    Changes in leaderboard

--- The Industrial Chemist/Controllers/LeaderboardAllTimeViewController.swift
+++ The Industrial Chemist/Controllers/LeaderboardAllTimeViewController.swift
@@ -9,13 +9,32 @@ import UIKit
 
 class LeaderboardAllTimeViewController: UIViewController {
 
+    @IBOutlet weak var leaderboardSegmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                // All-time selected → stay on this VC
+                print("All-Time Leaderboard")
+            case 1:
+                // Weekly selected → navigate to WeeklyLeaderboardVC
+                navigateToWeeklyLeaderboard()
+            default:
+                break
+            }
+    }
+    func navigateToWeeklyLeaderboard() {
+        print("Change page")
+    }
+    
 
+    
     /*
     // MARK: - Navigation
 
