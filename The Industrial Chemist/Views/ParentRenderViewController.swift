0b0dbe4f6cdc62731d881f03c089b2651b403782
anshggss <ptmtripathi@gmail.com>
2025-12-18 17:41:08 +0530

    Changes in UI

---
+++ The Industrial Chemist/Views/ParentRenderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ParentRenderViewController.swift
+//  The Industrial Chemist
+//
+//  Created by admin25 on 17/12/25.
+//
+
+import UIKit
+
+class ParentRenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
