5d6e69cbc2dedde03013d5ec248c2c0db35ba440
anshggss <ptmtripathi@gmail.com>
2026-01-09 06:10:31 +0530

    changes in home screen and leaderboard

---
+++ The Industrial Chemist/Models/AppColors.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+enum AppColors {
+    static let background = UIColor(hex: "#1A0129")
+    static let cardPrimary = UIColor(hex: "#E6C9FF")
+    static let textPrimary = UIColor(hex: "#1A0129")
+    static let icon = UIColor(hex: "#1A0129")
+}
---
+++ The Industrial Chemist/Models/LeaderBoardFile.swift
@@ -0,0 +1,15 @@
+//
+//  LeaderBoardFile.swift
+//  The Industrial Chemist
+//
+//  Created by admin25 on 09/01/26.
+//
+
+import UIKit
+
+struct LeaderboardEntry {
+    let rank: Int
+    let name: String
+    let xp: Int
+    let profileImage: UIImage?
+}
