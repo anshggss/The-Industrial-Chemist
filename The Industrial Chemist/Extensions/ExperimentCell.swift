e589ab7734eeceba4b82a6ecd5d33f886579c4f6
anshggss <ptmtripathi@gmail.com>
2026-01-30 15:45:25 +0530

    Changed UI

---
+++ The Industrial Chemist/Extensions/ExperimentCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExperimentCell.swift
+//  The Industrial Chemist
+//
+//  Created by admin25 on 28/01/26.
+//
+
+import UIKit
+
+class ExperimentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
