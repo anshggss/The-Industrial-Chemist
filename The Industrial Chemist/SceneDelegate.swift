e9d987f9d5b50895184b0d8f2ac05da8e0b74dba
Anushka1804 <anushkaagarwal180405@gmail.com>
2026-02-05 19:48:37 +0530

    made changes in the gas prep page

--- The Industrial Chemist/SceneDelegate.swift
+++ The Industrial Chemist/SceneDelegate.swift
@@ -1,46 +1,29 @@
 import UIKit
-import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        // 1. Capture the window scene
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        // 2. Initialize the window
         let window = UIWindow(windowScene: windowScene)
-//        let homeVC = HomeScreenNativeViewController(nibName: "HomeScreenNative", bundle: nil)
-        
-        // 3. Set the initial root to SplashViewController.
-        // This prevents the "Login Screen Flicker" because the Splash screen
-        // will stay visible until the logic decides where to go next.
-        
-        
-//        let navController = UINavigationController(rootViewController: homeVC)
-//        navController.navigationBar.prefersLargeTitles = true
-        window.rootViewController = /*navController*/ SplashViewController()
+
+        let rootVC = GasPrepViewController(
+            nibName: "GasPrep",
+            bundle: nil
+        )
+
+        let navigationController = UINavigationController(
+            rootViewController: rootVC
+        )
+
+        window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
     }
-
-    /// Call this method from anywhere (Login, Logout, Onboarding) to swap the root view controller smoothly.
-    func changeRootViewController(_ vc: UIViewController, animated: Bool = true) {
-        guard let window = self.window else { return }
-        
-        window.rootViewController = vc
-        
-        if animated {
-            UIView.transition(with: window,
-                              duration: 0.3,
-                              options: [.transitionCrossDissolve],
-                              animations: nil,
-                              completion: nil)
-        }
-    }
 }
+
